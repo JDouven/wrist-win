01a497aed2e10fd7058fa21e704a223a4c3547fb
Joris Douven <jdouven@ilionx.com>
2023-06-14 17:52:24 +0200

    add ios app

---
+++ ios/wrist-win Watch App/wrist_winApp.swift
@@ -0,0 +1,17 @@
+//
+//  wrist_winApp.swift
+//  wrist-win Watch App
+//
+//  Created by Joris Douven on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct wrist_win_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
